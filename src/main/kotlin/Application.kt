c5ed4f5b274fde3940faefa4ade24db5c404b34d
alexander <2.com9761721@gmail.com>
2025-04-09 13:15:52 +0300

    explicitly set pool sizes

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -2,26 +2,39 @@ package miet.lambda
 
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.install
+import io.ktor.server.engine.connector
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.calllogging.CallLogging
 import miet.lambda.db.PostgresDatabaseDataProvider
 
 fun main() {
-    embeddedServer(Netty, port = 8081, host = "::") {
-        install(CallLogging)
-        install(io.ktor.server.plugins.contentnegotiation.ContentNegotiation) {
-            json()
-        }
+    embeddedServer(
+        Netty,
+        configure = {
+            connector {
+                host = "::"
+                port = 8081
+            }
+            connectionGroupSize = 4
+            workerGroupSize = 100
+            callGroupSize = 100
+        },
+        module = {
+            install(CallLogging)
+            install(io.ktor.server.plugins.contentnegotiation.ContentNegotiation) {
+                json()
+            }
 
-        val dataProvider = PostgresDatabaseDataProvider()
+            val dataProvider = PostgresDatabaseDataProvider()
 
-        val roundRobinPoolLambdaExecutorProvider = RoundRobinPoolLambdaExecutorProvider(dataProvider)
+            val roundRobinPoolLambdaExecutorProvider = RoundRobinPoolLambdaExecutorProvider(dataProvider)
 
-        val lambdaExecutorProvider = SingleLambdaExecutorProvider(
-            lambdaExecutor = RetryingLambdaExecutor(roundRobinPoolLambdaExecutorProvider),
-        )
+            val lambdaExecutorProvider = SingleLambdaExecutorProvider(
+                lambdaExecutor = RetryingLambdaExecutor(roundRobinPoolLambdaExecutorProvider),
+            )
 
-        configureRouting(dataProvider, lambdaExecutorProvider)
-    }.start(wait = true)
+            configureRouting(dataProvider, lambdaExecutorProvider)
+        },
+    ).start(wait = true)
 }
